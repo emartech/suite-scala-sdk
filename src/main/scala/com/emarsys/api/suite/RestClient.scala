2589db271b707ccf3cac3e41a9f8bd94043c12c2
waff <vFeher@emarsys.com>
2016-09-06 12:51:27 +0200

    [refactor] extract rest client

---
+++ src/main/scala/com/emarsys/api/suite/RestClient.scala
@@ -0,0 +1,40 @@
+package com.emarsys.api.suite
+
+import scala.concurrent.{ExecutionContextExecutor, Future}
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+import akka.stream.Materializer
+import akka.stream.scaladsl.{Flow, Sink, Source}
+import com.emarsys.escher.akka.http.EscherDirectives
+import fommil.sjs.FamilyFormats._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+
+trait RestClient extends EscherDirectives {
+  implicit val system:       ActorSystem
+  implicit val materializer: Materializer
+  implicit val executor:     ExecutionContextExecutor
+
+  val connectionFlow: Flow[HttpRequest, HttpResponse, _]
+  val serviceName: String
+
+  protected def sendRequest(request: HttpRequest): Future[HttpResponse] = {
+    Source.single(request).via(connectionFlow).runWith(Sink.head)
+  }
+
+  def run[S](request: HttpRequest)(implicit um: Unmarshaller[ResponseEntity, S]): Future[S] = {
+    for {
+    signed   <- signRequest(serviceName)(executor, materializer)(request)
+    response <- sendRequest(signed)
+    result   <- response.status match {
+            case OK     => Unmarshal(response.entity).to[S]
+            case status => Unmarshal(response.entity).to[String].map { responseBody =>
+              system.log.error("Request to {} failed with status: {} / body: {}", request.uri, status, responseBody)
+              throw new Exception(s"Rest client request failed for ${request.uri}")
+            }
+          }
+    } yield result
+  }
+}
--- src/main/scala/com/emarsys/api/suite/SuiteClient.scala
+++ src/main/scala/com/emarsys/api/suite/SuiteClient.scala
@@ -1,48 +1,18 @@
 package com.emarsys.api.suite
 
-import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.model.headers.RawHeader
-import akka.stream.Materializer
-import akka.stream.scaladsl.{Flow, Sink, Source}
-import com.emarsys.escher.akka.http.EscherDirectives
-import fommil.sjs.FamilyFormats._
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.unmarshalling.{Unmarshal, Unmarshaller}
+import akka.stream.scaladsl.Flow
 import com.emarsys.api.Config.suiteConfig
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
+private[suite] trait SuiteClient extends RestClient {
 
-private[suite] trait SuiteClient extends EscherDirectives {
-
-  implicit val system:       ActorSystem
-  implicit val materializer: Materializer
-  implicit val executor:     ExecutionContextExecutor
-
+  val serviceName = suiteConfig.serviceName
   lazy val connectionFlow: Flow[HttpRequest, HttpResponse, _] = Http().outgoingConnectionHttps(suiteConfig.host)
-
-  protected def sendRequest(request: HttpRequest): Future[HttpResponse] = {
-    Source.single(request).via(connectionFlow).runWith(Sink.head)
-  }
 
   protected def createCustomerHeader(customerId: Int) = RawHeader("X-SUITE-CUSTOMERID", customerId.toString)
 
   def baseUrl(customerId: Int) =
     s"https://${suiteConfig.host}:${suiteConfig.port}${suiteConfig.suiteApiPath}/$customerId/"
-
-  def run[S](request: HttpRequest)(implicit um: Unmarshaller[ResponseEntity, S]): Future[S] = {
-    for {
-    signed   <- signRequest(suiteConfig.serviceName)(executor, materializer)(request)
-    response <- sendRequest(signed)
-    result   <- response.status match {
-            case OK => Unmarshal(response.entity).to[S]
-            case status => Unmarshal(response.entity).to[String].map { responseBody =>
-              system.log.error("Request to {} failed with status: {} / body: {}", request.uri, status, responseBody)
-              throw new Exception(s"Suite client request failed for ${request.uri}")
-            }
-          }
-    } yield result
-  }
 }
